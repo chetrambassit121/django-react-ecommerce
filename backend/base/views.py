779ba707acb3be37ba9e215530b22a0f361d5adb
Chetram Bassit <chetrambassit121@gmail.com>
2023-04-18 12:24:34 -0400

    installed drf

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 # Create your views here.
 
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = ['api routes']
+    return JsonResponse(routes, safe=False)
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
+
