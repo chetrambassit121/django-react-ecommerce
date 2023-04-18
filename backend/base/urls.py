e4535e583f11e3aab7afd7d8a7234ff56e029b4e
Chetram Bassit <chetrambassit121@gmail.com>
2023-04-18 13:04:11 -0400

    getProduct for returning product details

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,4 +4,34 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
+    path('products/<str:pk>/', views.getProduct, name="product"),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ............. after creating the views . created home path as routes this displays all our routes . products path displays json data on all products 
+
+# from django.urls import path 
+# from . import views 
+
+# urlpatterns = [
+#     path('', views.getRoutes, name="routes"),
+#     path('products/', views.getProducts, name="products"),
+# ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -27,6 +27,68 @@ def getRoutes(request):
 def getProducts(request):
     return Response(products)
 
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    
+    ''' get the products _id , matching it with the products pk, then breaking '''
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+        
+    return Response(product)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ................... importing RF decorator / response ...  adding @apiview decoraters on the FBV's . the decorator is for GET getting data . we can now view routes on the DRF interface   
+
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from .products import products
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# # Create your views here.
+
+# @api_view(['GET'])
+# def getRoutes(request):
+#     routes = [
+#         '/api/products/',
+#         '/api/products/create/',
+        
+#         '/api/products/upload',
+        
+#         '/api/products/<id>/reviews/'
+        
+#         '/api/products/top/',
+#         '/api/products/<id>/',
+        
+#         '/api/products/delete/<id>/',
+#         '/api/products/<update>/<id>/',
+#     ]
+#     return Response(routes)
+
+# @api_view(['GET'])
+# def getProducts(request):
+#     return Response(products)
+
 
 
 
