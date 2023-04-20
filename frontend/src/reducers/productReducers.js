75556b2f8f39fec397f9b077b13efe78d08e2180
Chetram Bassit <chetrambassit121@gmail.com>
2023-04-20 12:02:42 -0400

    applied react-redux and listProducts function to home screen, prodcuts are now displayed

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,19 +4,19 @@ import {
     PRODUCT_LIST_FAIL
 } from '../constants/productConstants'
 
-export const productListReducer = (state ={ products:[] }, action) => {   
+export const productListReducer = (state = { products:[] }, action) => {   
     switch(action.type){                                         
         case PRODUCT_LIST_REQUEST:                               
-        return {loading:true, product:[]}                    
+        return {loading:true, products:[]}                    
 
         case PRODUCT_LIST_SUCCESS:                             
-        return {loading:false, product:action.payload}       
+        return {loading:false, products:action.payload}       
 
         case PRODUCT_LIST_FAIL:                                
         return {loading:false, error:action.payload}
 
         default:
-        return state
+            return state
     }
 }  
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,27 +1,20 @@
-
+// implementing redux, implmenting out listProducts to return the prodycts to our home screen. 
 
 import React, {useState, useEffect} from 'react'
-// import products from '../products'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'                      // using axios to make calls and load data then update 
+import {useDispatch, useSelector} from 'react-redux'     
+import  {listProducts}  from '../actions/productActions'
 
 function HomeScreen() {
-
-  // constant [products (our state is called products), setProducts (method for updating the state)] bound to useState([empty array]) ... this is our set up for using useState 
-  const [products, setProducts] = useState([])   
-
-  // using useEffect to load our products . it gets triggered everytime a component loads or if a state value gets updated .
-  // its a arrow function => . 
+  const dispatch = useDispatch()
+  const productList = useSelector(state => state.productList)
+  const {error, loading, products} = productList
   useEffect(() => {
-    async function fetchProducts(){                                                     // creating the async function ... 
-      const {data} = await axios.get('/api/products/')               // constant data . awaiting for axios getting the url 
-      setProducts(data)                         
-    }
-    fetchProducts()                                       
-  }, [])                                                                                // [empty array] b/c we only want update when a component loads not when a state element gets updated 
+    dispatch(listProducts())
+  }, [])                                                                                
 
-    
+  // const products = []
 
   return (
     <div>
@@ -38,6 +31,78 @@ function HomeScreen() {
 }
 
 export default HomeScreen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {useState, useEffect} from 'react'
+// // import products from '../products'
+// import { Row, Col } from 'react-bootstrap'
+// import Product from '../components/Product'
+// import axios from 'axios'                      // using axios to make calls and load data then update 
+
+// function HomeScreen() {
+
+//   // constant [products (our state is called products), setProducts (method for updating the state)] bound to useState([empty array]) ... this is our set up for using useState 
+//   const [products, setProducts] = useState([])   
+
+//   // using useEffect to load our products . it gets triggered everytime a component loads or if a state value gets updated .
+//   // its a arrow function => . 
+//   useEffect(() => {
+//     async function fetchProducts(){                                                     // creating the async function ... 
+//       const {data} = await axios.get('/api/products/')               // constant data . awaiting for axios getting the url 
+//       setProducts(data)                         
+//     }
+//     fetchProducts()                                       
+//   }, [])                                                                                // [empty array] b/c we only want update when a component loads not when a state element gets updated 
+
+    
+
+//   return (
+//     <div>
+//       <h1>Latest Products</h1>
+//       <Row>
+//         {products.map(product => (
+//           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+//             <Product product={product}/>
+//           </Col>
+//         ))}
+//       </Row>
+//     </div>
+//   )
+// }
+
+// export default HomeScreen
 
 
 
