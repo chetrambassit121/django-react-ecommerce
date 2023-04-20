ce1b8de47257413b18d8fadd791c672981b133a5
Chetram Bassit <chetrambassit121@gmail.com>
2023-04-20 12:42:30 -0400

    Loading and Message components created with react-bootsrap, attached components to Homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -5,6 +5,8 @@ import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import {useDispatch, useSelector} from 'react-redux'     
 import  {listProducts}  from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function HomeScreen() {
   const dispatch = useDispatch()
@@ -19,8 +21,8 @@ function HomeScreen() {
   return (
     <div>
       <h1>Latest Products</h1>
-      {loading ? <h2>Loading...</h2>
-        : error ? <h3>{error}</h3>
+      {loading ? <Loader/>
+        : error ? <Message variant='danger'>{error}</Message>
           :
             <Row>
               {products.map(product => (
