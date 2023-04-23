70b0ac4fa464a9a52803daa7115faaa1e2a62081
Chetram Bassit <chetrambassit121@gmail.com>
2023-04-23 16:13:20 -0400

    userRegister constants, reduces, actions, connected to store and app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/login' component={LoginScreen} exact />
+          <Route path='/register' component={RegisterScreen} exact />
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
         </Container>
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,7 +5,9 @@ import {
     USER_LOGIN_FAIL,
     USER_LOGOUT,
 
-    USER_DETAILS_RESET,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
 
 } from '../constants/userConstants'
 
@@ -54,3 +56,42 @@ export const logout = () => (dispatch) => {
 }
 
 
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post(
+            '/api/users/register/',
+            { 'name': name, 'email': email, 'password': password },
+            config
+        )
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
