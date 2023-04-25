4867bc9fe7226ef7caca737377df5ec3708fc7ec
Chetram Bassit <chetrambassit121@gmail.com>
2023-04-25 13:45:29 -0400

    place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
 
 function App() {
@@ -22,11 +23,12 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path='/' component={HomeScreen} exact />
-          <Route path='/login' component={LoginScreen} exact />
-          <Route path='/register' component={RegisterScreen} exact />
-          <Route path='/profile' component={ProfileScreen} exact />
-          <Route path='/shipping' component={ShippingScreen} exact />
-          <Route path='/payment' component={PaymentScreen} exact />
+          <Route path='/login' component={LoginScreen}/>
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
         </Container>
