27caf31223f841ace931cc107e1286417a6a6e27
Chetram Bassit <chetrambassit121@gmail.com>
2023-05-02 22:59:46 -0400

    aws s3

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:8000', 'chets.animeshop.herokuapp.com']
 
@@ -185,6 +185,33 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+
+# AWS_ACCESS_KEY_ID = "AKIAQV3OTVOKSPVX46UR"
+# AWS_SECRET_ACCESS_KEY = "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"
+# AWS_STORAGE_BUCKET_NAME = "chetsanimeshop"
+
+
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+# AWS_DEFAULT_ACL = 'public-read'
+
+# AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+# AWS_LOCATION = 'static'
+
+# AWS_QUERYSTRING_AUTH = False
+
+# AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
+
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+# STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
 # Static files (CSS, JavaScript, Images)
