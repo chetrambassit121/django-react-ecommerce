91d227df94f5110f8ecfd24b6151d96b9b4d2d80
Chetram Bassit <chetrambassit121@gmail.com>
2023-05-04 13:46:02 -0400

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # "whitenoise.runserver_nostatic",    
+    "whitenoise.runserver_nostatic",    
     'rest_framework',
     'corsheaders',
     'storages',
