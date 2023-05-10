0f0dcb97830b8756e24068fd200f5094c77d1e39
Chetram Bassit <chetrambassit121@gmail.com>
2023-05-10 15:59:03 -0400

    background video

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 
 function Product({ product }) {
     return (
-        <Card className="my-3 p-3 rounded">
+        <Card className="my-3 p-3 rounded productcard">
             <Link to={`/product/${product._id}`}>
-                <Card.Img src={product.image} />
+                <Card.Img src={product.image} min-height="200px"/>
             </Link>
 
             <Card.Body>
@@ -22,7 +22,6 @@ function Product({ product }) {
                         <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
                     </div>
                 </Card.Text>
-
 
                 <Card.Text as="h3">
                     ${product.price}
