6a3acb4ddba5bc62accab48edc084e01db4b4fd4
Chetram Bassit <chetrambassit121@gmail.com>
2023-05-11 10:49:01 -0400

    statifiles

--- backend/settings.py
+++ backend/settings.py
@@ -243,7 +243,7 @@ AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 
 
 if os.getcwd() == '/app':
-    DEBUG = False
+    DEBUG = True
     
 django_on_heroku.settings(locals())
 options = DATABASES['default'].get('OPTIONS', {})
