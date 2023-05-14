ac0482ef34efa5a3476d4a5646799a62abc2deee
Chetram Bassit <chetrambassit121@gmail.com>
2023-05-13 20:00:59 -0400

    form

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,8 +6,17 @@ function Footer() {
         <footer>
             <Container>
                 <Row>
+                    <Col className="text-center py-3">Animated background from <a href="https://moewalls.com/" target='_blank'>moewalls.com</a></Col>
                     <Col className="text-center py-3">Copyright &copy; Chets Anime Shop</Col>
+                    <Col className="text-center py-3">Photos from <a href="https://wallpapercave.com/" target='_blank'>wallpapercave.com</a></Col>
+
                 </Row>
+                {/* <Row>
+                    <Col className="text-center py-3">Photos from <a href="https://wallpapercave.com/" target='_blank'>wallpapercave.com</a></Col>
+                </Row>
+                <Row>
+                    <Col className="text-center py-3">Photos from <a href="https://wallpapercave.com/" target='_blank'>wallpapercave.com</a></Col>
+                </Row> */}
             </Container>
         </footer>
     )
